5cd956c4ecc1203619b9b3e5795db241a270eff3
Mohamed Esskely <messkely@e2r7p15.1337.ma>
2025-06-01 18:22:51 +0100

    done

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # include chat app routes here
+    path('api/', include('chats.urls')),  # Include your app's routes
+    path('api-auth/', include('rest_framework.urls')),  # Enables login/logout in browsable API
 ]
